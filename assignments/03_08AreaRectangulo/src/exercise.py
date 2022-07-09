63a6bfbc805b5e9680aa8539b2f1e9e551932a60
Laura_Munoz <45415865+lauramh018@users.noreply.github.com>
2022-07-09 12:46:09 -0500

    Update exercise.py

--- assignments/03_08AreaRectangulo/src/exercise.py
+++ assignments/03_08AreaRectangulo/src/exercise.py
@@ -1,8 +1,13 @@
-#Escribe aqui tus funciones
+base = float(input('Dame la base:'))
+altura = float(input('Dame la altura:'))
+
+def area_rectangulo(base,altura):
+    area = base * altura
+    return area
 
 def main() :
     #escribe tu código abajo de esta línea
-    pass
+    print (f'El área del rectámgulo es: {area_rectangulo(base,altura)}')
 
 
 
